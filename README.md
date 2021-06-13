ad526a8a48bfc6f965689e4c22357c445c393325
perelandrax <luvyum4e@gmail.com>
2021-06-13 09:56:26 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LifecycleSample
+
+* [Medium - 생각지도 못했던 안드로이드 생명주기 면접질문](https://perelandrax.medium.com/%EC%83%9D%EA%B0%81%EC%A7%80%EB%8F%84-%EB%AA%BB%ED%96%88%EB%8D%98-%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%83%9D%EB%AA%85%EC%A3%BC%EA%B8%B0-%EB%A9%B4%EC%A0%91%EC%A7%88%EB%AC%B8-a34b19895d83)
--- app/src/main/java/com/perelandrax/lifecyclesample/AActivity.kt
+++ app/src/main/java/com/perelandrax/lifecyclesample/AActivity.kt
@@ -8,48 +8,48 @@ import androidx.appcompat.app.AppCompatActivity
 
 class AActivity : AppCompatActivity() {
 
-    companion object {
-        private var TAG = AActivity::class.java.simpleName
+  companion object {
+    private var TAG = AActivity::class.java.simpleName
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setContentView(R.layout.activity_a)
+    Log.d(TAG, "[AActivity] onCreate()")
+
+    findViewById<Button>(R.id.btn_startactivity_a_to_b).setOnClickListener {
+      startActivity(Intent(this, BActivity::class.java))
     }
+  }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  override fun onRestart() {
+    super.onRestart()
+    Log.d(TAG, "[AActivity] onRestart()")
+  }
 
-        setContentView(R.layout.activity_a)
-        Log.d(TAG, "[AActivity] onCreate()")
+  override fun onStart() {
+    super.onStart()
+    Log.d(TAG, "[AActivity] onStart()")
+  }
 
-        findViewById<Button>(R.id.btn_startactivity_a_to_b).setOnClickListener {
-            startActivity(Intent(this, BActivity::class.java))
-        }
-    }
+  override fun onResume() {
+    super.onResume()
+    Log.d(TAG, "[AActivity] onResume()")
+  }
 
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "[AActivity] onRestart()")
-    }
+  override fun onPause() {
+    super.onPause()
+    Log.d(TAG, "[AActivity] onPause()")
+  }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "[AActivity] onStart()")
-    }
+  override fun onStop() {
+    super.onStop()
+    Log.d(TAG, "[AActivity] onStop()")
+  }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "[AActivity] onResume()")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "[AActivity] onPause()")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "[AActivity] onStop()")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "[AActivity] onDestroy()")
-    }
+  override fun onDestroy() {
+    super.onDestroy()
+    Log.d(TAG, "[AActivity] onDestroy()")
+  }
 }
